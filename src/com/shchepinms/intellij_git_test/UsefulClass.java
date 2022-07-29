f6b977ef7163e3c205d6d298fac4c33ee4daf489
MS-Shchepin <promayhem@mail.ru>
2022-07-29 15:00:33 +0300

    Some changes added

--- src/com/shchepinms/intellij_git_test/UsefulClass.java
+++ src/com/shchepinms/intellij_git_test/UsefulClass.java
@@ -3,5 +3,6 @@ package com.shchepinms.intellij_git_test;
 public class UsefulClass {
     public static void main(String[] args) {
         System.out.println("Very useful class :D");
+        System.out.println("Add some changes...");
     }
 }
